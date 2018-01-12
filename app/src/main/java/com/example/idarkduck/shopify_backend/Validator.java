03f407617b4909cbdd4e641510306ed9311a20ec
iDuckDark <ngane103@uottawa.ca>
2018-01-11 21:16:15 -0500

    trying to validate using tree nodes
    mss

---
+++ app/src/main/java/com/example/idarkduck/shopify_backend/Validator.java
@@ -0,0 +1,17 @@
+package com.example.idarkduck.shopify_backend;
+
+import java.util.ArrayList;
+
+/**
+ * Created by iDarkDuck on 1/11/18.
+ */
+
+public class Validator {
+
+    ArrayList<Menu> menus;
+
+    Validator(ArrayList<Menu> menus){
+        this.menus=menus;
+    }
+    
+}
