68d0b84d80a8e31e343effd0dab1dcbad1395a1e
iDuckDark <ngane103@uottawa.ca>
2018-01-11 21:33:31 -0500

    sample

--- app/src/main/java/com/example/idarkduck/shopify_backend/Validator.java
+++ app/src/main/java/com/example/idarkduck/shopify_backend/Validator.java
@@ -9,9 +9,37 @@ import java.util.ArrayList;
 public class Validator {
 
     ArrayList<Menu> menus;
+    TreeNode<String> treeNode;
 
     Validator(ArrayList<Menu> menus){
         this.menus=menus;
+
     }
-    
-}
+
+    private void buildTree(){
+
+        for(int i=0; i<menus.size(); i++){
+            if(treeNode==null){
+                treeNode = new TreeNode<>(menus.get(0).getId());
+            }
+            else{
+                treeNode.addChild(menus.get(i).getId());
+            }
+        }
+    }
+
+    Comparable<String> searchCriteria = new Comparable<String>() {
+        @Override
+        public int compareTo(String treeData) {
+            if (treeData == null)
+                return 1;
+            boolean nodeOk = treeData.contains("210");
+            return nodeOk ? 0 : 1;
+        }
+    };
+
+
+
+    }
+
+
