fb872dbf56555ac61cca02e547f25178b8222ff8
Nick <nbroa025@uottawa.ca>
2018-01-11 23:04:11 -0500

    valid json

--- app/src/main/java/com/example/idarkduck/shopify_backend/ResponseJson.java
+++ app/src/main/java/com/example/idarkduck/shopify_backend/ResponseJson.java
@@ -1,20 +1,50 @@
 package com.example.idarkduck.shopify_backend;
 
 import java.util.ArrayList;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 /**
  * Created by Nick-JR on 1/11/2018.
  */
 
 class ResponseJson {
+    String jsonString;
+    JSONObject valid;
+
+    public ResponseJson() {
+        try {
+            valid = new JSONObject();
+            jsonString = new JSONObject()
+                        .put("valid_menus", new JSONArray()).toString();
+        } catch (JSONException e) {
+
+        }
+    }
 
     // Sets the menu of an item key valid.
-    protected void setValidMenu(ArrayList<Menu> menu) {
+    protected void addValidMenu(ArrayList<Menu> menu) {
+        try {
+            valid.put("root_id", menu.get(0).getId());
+            JSONArray children = new JSONArray();
 
+            for (int i = 1; i < menu.size(); i++) {
+                children.put(i - 1, menu.get(i).getId());
+            }
+            valid.put("children", children);
+
+            jsonString = new JSONObject().put("valid_menus", valid).toString();
+
+
+            System.out.println(jsonString);
+        } catch (JSONException e) {
+            System.out.println(e);
+        }
     }
 
     // Sets the menu of an item key invalid.
-    protected void setInvalidMenu(ArrayList<Menu> menu) {
+    protected void addInvalidMenu(ArrayList<Menu> menu) {
 
     }
 }
